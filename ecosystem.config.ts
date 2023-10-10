7cd7967b2d9736c2c89ecc68e77df49b8d9dfc2a
hariscs4997 <hariscs4997@gmail.com>
2023-10-10 18:57:16 +0500

    changes

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -1,21 +1,26 @@
 module.exports = {
   apps: [
     {
-      script: 'npm start',
+      script: 'index.js',
+      watch: '.',
+    },
+    {
+      script: './service-worker/',
+      watch: ['./service-worker'],
     },
   ],
+
   deploy: {
     production: {
       user: 'ubuntu',
       host: 'app.g-datalabs.com',
-      ref: 'origin/main',
+      ref: 'origin/master',
       repo: 'git@github.com:Shehzer-Aurangzeb/G-DataLabs.git',
       path: '/home/ubuntu',
       'pre-deploy-local': '',
       'post-deploy':
         'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.ts --env production',
       'pre-setup': '',
-      ssh_options: 'ForwardAgent=yes',
     },
   },
 };
