32637922a85940964388c62625398e5b26efb32c
Shehzer-Aurangzeb <shehzerabbasi621@gmail.com>
2023-11-06 18:24:40 +0500

    error handling added

--- context/AppProvider.tsx
+++ context/AppProvider.tsx
@@ -2,6 +2,7 @@
 
 import React, { createContext, useEffect, useContext, useCallback, useState, useMemo } from 'react';
 import { Column } from 'react-table';
+import { AxiosError } from 'axios';
 import { usePersonalData } from '@/state/myGData/hooks';
 import { api } from '@/config';
 import { useUser } from '@/state/user/hooks';
@@ -9,6 +10,7 @@ import { createScreenData, createTableColumns, createTableData } from '@/lib';
 import { Columns, TableName } from '@/types';
 import { useWeather } from '@/hooks/useWeather';
 import { useChatBot } from '@/hooks/useChatBot';
+import { useAuth } from '@/hooks/useAuth';
 
 type AppContextType = {
   gTableColumns: Column<Columns>[];
@@ -32,35 +34,63 @@ function AppProvider({ children }: IProps) {
   const { fetchChatHistory, fetchRecentChats } = useChatBot();
   const [gTableColumns, setGTableColumns] = useState<Column<Columns>[]>([]);
   const { user } = useUser();
+  const { logoutUser } = useAuth();
 
   //* weather hook
   useWeather();
   //* functions
   const getAllPersonalData = useCallback(async () => {
-    const { data } = await api.get('api/personal_data_consents_rewards');
-    const PData = createTableData({ tableName: TableName.PData, data: data.data });
-    setPersonalData(PData);
-  }, [setPersonalData]);
+    try {
+      const { data } = await api.get('api/personal_data_consents_rewards');
+      const PData = createTableData({ tableName: TableName.PData, data: data.data });
+      setPersonalData(PData);
+    } catch (e) {
+      if (
+        e instanceof AxiosError &&
+        (e.response?.status === 401 || e.response?.data.msg === 'Token has expired') &&
+        user
+      ) {
+        logoutUser();
+      }
+    }
+  }, [setPersonalData, logoutUser, user]);
 
   const getAllConsentData = useCallback(async () => {
-    const { data } = await api.get('api/user_consents_rewards');
-    const rData = createTableData({ tableName: TableName.RData, data: data.data });
-    const consentTableData = createTableData({ tableName: TableName.CData, data: data.data });
-    setRData(rData);
-    setCData(consentTableData);
-  }, [setRData, setCData]);
+    try {
+      const { data } = await api.get('api/user_consents_rewards');
+      const rData = createTableData({ tableName: TableName.RData, data: data.data });
+      const consentTableData = createTableData({ tableName: TableName.CData, data: data.data });
+      setRData(rData);
+      setCData(consentTableData);
+    } catch (e) {
+      if (
+        e instanceof AxiosError &&
+        (e.response?.status === 401 || e.response?.data.msg === 'Token has expired') &&
+        user
+      ) {
+        logoutUser();
+      }
+    }
+  }, [setRData, setCData, logoutUser, user]);
 
   const getLastFivePersonalData = useCallback(async () => {
-    const { data } = await api.get('api/user_personal_data/last_five');
-    const gData = createTableData({ tableName: TableName.GData, data });
-    if (data.length === 0) return;
-    const gDataTableColumns = createTableColumns(data);
-    console.log('GData columns', gDataTableColumns);
-    console.log('GData ', gData);
-
-    setGTableColumns(gDataTableColumns);
-    setGData(gData);
-  }, [setGData]);
+    try {
+      const { data } = await api.get('api/user_personal_data/last_five');
+      const gData = createTableData({ tableName: TableName.GData, data });
+      if (data.length === 0) return;
+      const gDataTableColumns = createTableColumns(data);
+      setGTableColumns(gDataTableColumns);
+      setGData(gData);
+    } catch (e) {
+      if (
+        e instanceof AxiosError &&
+        (e.response?.status === 401 || e.response?.data.msg === 'Token has expired') &&
+        user
+      ) {
+        logoutUser();
+      }
+    }
+  }, [setGData, logoutUser, user]);
 
   const getAllScreenData = useCallback(async () => {
     try {
@@ -68,9 +98,15 @@ function AppProvider({ children }: IProps) {
       const screenData = createScreenData(data.data);
       setScreenData(screenData);
     } catch (e) {
-      // console.log('e :>> ', e);
+      if (
+        e instanceof AxiosError &&
+        (e.response?.status === 401 || e.response?.data.msg === 'Token has expired') &&
+        user
+      ) {
+        logoutUser();
+      }
     }
-  }, [setScreenData]);
+  }, [setScreenData, logoutUser, user]);
 
   const initApp = useCallback(() => {
     getAllPersonalData();
